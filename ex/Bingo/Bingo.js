d6281eabd86b3259e8ffe23bb87583e28ba042b1
shrfrn <shrfrn@gmail.com>
2024-11-28 10:57:27 +0200

    CR

--- ex/Bingo/Bingo.js
+++ ex/Bingo/Bingo.js
@@ -1,162 +1,153 @@
 'use strict'
+
 const ROWS = 5
 const COLS = 5
+
 const SMALLEST_NUMBER = 1
 const LARGEST_NUMBER = 100
+
 const gPlayers = [
-	{ name: 'Mebahem', card: createCard(ROWS,COLS) },
-	{ name: 'Alberg', card: createCard(ROWS,COLS) },
+	{ name: 'Mebahem', card: createCard(ROWS, COLS) },
+	{ name: 'Alberg', card: createCard(ROWS, COLS) },
 ]
-var gLots = initArrey(SMALLEST_NUMBER, LARGEST_NUMBER)
+
+var gLots = initArray(SMALLEST_NUMBER, LARGEST_NUMBER)
+
 const gIntrvalId = setInterval(startBingo, 300)
 
 function shuffle(array) {
-    let currentIndex = array.length;
-  
-    // While there remain elements to shuffle...
-    while (currentIndex != 0) {
-  
-      // Pick a remaining element...
-      let randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-    return array
-}
-  
-function initArrey(from, to){
-    var lst = []
-    for (var i=from; i<=to; i++){
-        lst.push(i)
-    }
-    return shuffle(lst)
+	let currentIndex = array.length
+
+	// While there remain elements to shuffle...
+	while (currentIndex != 0) {
+		// Pick a remaining element...
+		let randomIndex = Math.floor(Math.random() * currentIndex)
+		currentIndex--
+
+		// And swap it with the current element.
+		;[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
+	}
+	return array
 }
 
-function createCard(x,y) {
-    var row = []
-    var card = []
-    var numbers = initArrey(SMALLEST_NUMBER, LARGEST_NUMBER)
-    var cell = {
-        value: null,       // The number in the cell
-        isHit: false,   // true if that number has been called
-    }
+function initArray(from, to) {
+	var nums = []
 
-    for (var i=0; i<x;i++){
-        for (var j=0; j<y; j++){
-            row[j] = structuredClone(cell)
-            row[j].value = getUniqueRandom(numbers) 
-        }
-        card[i] = row
-        row = []
-    }
+	for (var i = from; i <= to; i++) {
+		nums.push(i)
+	}
+	return shuffle(nums)
+}
 
-    return card
+function createCard(rows, cols) {
+	var card = []
+	var numbers = initArray(SMALLEST_NUMBER, LARGEST_NUMBER)
+
+	for (var i = 0; i < rows; i++) {
+        card[i] = []
+		for (var j = 0; j < cols; j++) {
+			card[i][j] = { value: numbers.pop(), isHit: false }
+		}
+	}
+	return card
 }
 
 function getUniqueRandom(preDefinedList) {
-    return preDefinedList.pop()
+	return preDefinedList.pop()
 }
 
 function printCard(card) {
-    var printableCard = []
-    for (var i = 0; i < card.length; i++){
-        printableCard[i] = []
-        for (var j = 0; j < card[i].length; j++){
-            const prefix = card[i][j].isHit ? 'X' : ''
-            printableCard[i][j] = prefix + card[i][j].value
-        }
-    }
-    console.table(printableCard)
+	var printableCard = []
+
+	for (var i = 0; i < card.length; i++) {
+		printableCard[i] = []
+		for (var j = 0; j < card[i].length; j++) {
+			const prefix = card[i][j].isHit ? 'X' : ''
+			printableCard[i][j] = prefix + card[i][j].value
+		}
+	}
+	console.table(printableCard)
 }
 
-function markCard(card, number){
-    var currentNumber = null
-    for (var i=0; i<card.length;i++){
-        const row = card[i]
-        for (var j = 0; j < row.length; j++) {
-            currentNumber = row[j].value
-            if (currentNumber === number) {
-                row[j].isHit = true
-                break
-            }
-        }
-        if (currentNumber === number) {
-            break
-        }
-    }
-    return card
+function markCard(card, number) {
+	var currentNumber = null
+
+	for (var i = 0; i < card.length; i++) {
+		const row = card[i]
+		for (var j = 0; j < row.length; j++) {
+			currentNumber = row[j].value
+			if (currentNumber === number) return (row[j].isHit = true)
+		}
+	}
 }
 
-function checkBingo(card) {
-    if (checkRows(card) || 
-        checkCols(card) || 
-        checkDiag(card)) return true
+function checkBingo(card, i, j) {
+	if (checkRows(card, i) || checkCols(card, j) || checkDiag(card)) return true
 
-    return false
+	return false
 }
 
-function checkRows(card){
-    for (var i=0; i<card.length; i++){
-        var row = card[i]
-        if (row.every(cell => cell.isHit)) {
-            console.log(`Bingo found on row ${i}`)
-            return true
-        }
-    }
-    return false
+function checkRows(card) {
+	for (var i = 0; i < card.length; i++) {
+		var row = card[i]
+		if (row.every(cell => cell.isHit)) {
+			console.log(`Bingo found on row ${i}`)
+			return true
+		}
+	}
+	return false
 }
 
-function checkCols(card){
-    for (var i=0; i<card.length; i++){
-        var col = []
-        for (var j=0; j<card.length; j++){
-            const row = card[j]
-            col.push(row[i])
-        }
-        if (col.every(cell => cell.isHit)) {
-            console.log(`Bingo found on col ${i}`)
-            return true
-        }
-    }
-    return false
+function checkCols(card) {
+	// for(var i = 0; i < card.length; i++){
+	//     for(var j = 0; j < card[i].length; j++){
+	//         if (!card[j][i].isHit) return false
+	//     }
+	// }
+	// return true
+
+	for (var i = 0; i < card.length; i++) {
+		var col = []
+		for (var j = 0; j < card.length; j++) {
+			const row = card[j]
+			col.push(row[i])
+		}
+		if (col.every(cell => cell.isHit)) {
+			console.log(`Bingo found on col ${i}`)
+			return true
+		}
+	}
+	return false
 }
 
-function checkDiag(card){
-    var diag = []
-    for (var i=0; i<card.length; i++){
-        for (var j=0; j<card.length; j++){
-            if (i === j) diag.push(card[i][j])
-        }
-    }
-    if (diag.every(cell => cell.isHit)) {
-        console.log(`Bingo found on the diagonal`)
-        return true
-    }
-    
-    return false
+function checkDiag(card) {
+	for (var i = 0; i < card.length; i++) {
+		if (!card[i][i].isHit) return false
+	}
+	return true
 }
 
 function startBingo() {
-    var isBingo = playTurn()
-    if (isBingo) clearInterval(gIntrvalId)
+	var isBingo = playTurn()
+	if (isBingo) clearInterval(gIntrvalId)
 }
 
-function playTurn(){
-    var currentNumber = getUniqueRandom(gLots)
-    var bBingo = false
-    for (var i=0; i<gPlayers.length; i++){
-        const card = gPlayers[i].card
-        markCard(card, currentNumber)
-        console.log(`${gPlayers[i].name}'s card:`)
-        printCard(card)
-        bBingo = checkBingo(card)
-        if (bBingo){
-            console.log(`the winner is: ${gPlayers[i].name}`)
-            return bBingo
-        }
-    }
-    return bBingo
+function playTurn() {
+	var currentNumber = getUniqueRandom(gLots)
+	var isBingo = false
+
+	for (var i = 0; i < gPlayers.length; i++) {
+		const card = gPlayers[i].card
+
+		markCard(card, currentNumber)
+		console.log(`${gPlayers[i].name}'s card:`)
+		printCard(card)
+
+		isBingo = checkBingo(card)
+		if (isBingo) {
+			console.log(`the winner is: ${gPlayers[i].name}`)
+			return isBingo
+		}
+	}
+	return isBingo
 }
--- ex/Game of Life/Game of Life.js
+++ ex/Game of Life/Game of Life.js
@@ -1,100 +1,120 @@
 'use strict'
 const ROWS = 20
 const COLS = 20
+
 const SMALLEST_NUMBER = 0
 const LARGEST_NUMBER = 2
-var NUMBER_OF_GENERATIONS = 30
+
+var gNumOfGeneretions = 30
+
 var gGrid = createGrid(ROWS, COLS, SMALLEST_NUMBER, LARGEST_NUMBER)
 const gIntervalId = setInterval(startGame, 300)
 
+function createGrid(rows, cols, minNum, maxNum) {
+	var grid = []
 
-function createGrid(x, y, minNum, maxNum) {
-    var row = []
-    var grid = []
-    for (var i = 0; i < x; i++) {
-        for (var j = 0; j < y; j++) {
-            row[j] = getRandomInt(minNum, maxNum)
-        }
-        grid[i] = row
-        row = []
-    }
-    return grid
+	for (var i = 0; i < rows; i++) {
+		grid[i] = []
+		for (var j = 0; j < cols; j++) {
+			grid[i][j] = getRandomInt(minNum, maxNum)
+		}
+	}
+	return grid
 }
 
+// function createGrid(x, y, minNum, maxNum) {
+// 	var row = []
+// 	var grid = []
+
+// 	for (var i = 0; i < x; i++) {
+// 		for (var j = 0; j < y; j++) {
+// 			row[j] = getRandomInt(minNum, maxNum)
+// 		}
+// 		grid[i] = row
+// 		row = []
+// 	}
+// 	return grid
+// }
+
 function getRandomInt(min, max) {
-    if (min === max) return min
-    const minCeiled = Math.ceil(min)
-    const maxFloored = Math.floor(max)
-    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled) // The maximum is exclusive and the minimum is inclusive
+	if (min === max) return min
+	const minCeiled = Math.ceil(min)
+	const maxFloored = Math.floor(max)
+	return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled) // The maximum is exclusive and the minimum is inclusive
 }
 
 function printGrid(grid) {
-    console.clear()
     var printableGrid = []
-    for (var i = 0; i < grid.length; i++) {
-        printableGrid[i] = []
-        for (var j = 0; j < grid[i].length; j++) {
-            const displayVal = grid[i][j] ? '*' : ' '
-            printableGrid[i][j] = displayVal
-        }
-    }
-    console.table(printableGrid)
+
+	console.clear()
+	for (var i = 0; i < grid.length; i++) {
+		printableGrid[i] = []
+		for (var j = 0; j < grid[i].length; j++) {
+			printableGrid[i][j] = grid[i][j] ? '*' : ' '
+		}
+	}
+	console.table(printableGrid)
 }
 
 function getNextState(grid) {
-    var nextState = []
-    for (var i = 0; i < grid.length; i++) {
-        nextState[i] = []
-        for (var j = 0; j < grid[i].length; j++) {
-            var neighboursCount = countNeighbors(grid, i, j)
-            if (grid[i][j] && (neighboursCount === 2 || neighboursCount === 3)) nextState[i][j] = grid[i][j]
-            else if (!grid[i][j] && neighboursCount === 3) nextState[i][j] = 1
-            else nextState[i][j] = 0
-        }
-    }
-    return nextState
+	var nextState = []
+
+	for (var i = 0; i < grid.length; i++) {
+		nextState[i] = []
+		for (var j = 0; j < grid[i].length; j++) {
+			var negs = countNeighbors(grid, i, j)
+
+			if (negs === 3 || (grid[i][j] && negs === 2)) {
+				nextState[i][j] = 1
+			} else {
+                nextState[i][j] = 0
+			}
+		}
+	}
+	return nextState
 }
 
 function countNeighbors(grid, x, y) {
-    var neighboursAlive = 0
-    var max = grid.length - 1
-    var fromX = (x - 1 > 0) ? x - 1 : 0
-    var fromY = (y - 1 > 0) ? y - 1 : 0
-    var toX = (x + 1 <= max) ? x + 1 : max
-    var toY = (y + 1 <= max) ? y + 1 : max
-    for (var i = fromX; i <= toX; i++) {
-        for (var j = fromY; j <= toY; j++) {
-            if (i === x && j === y) continue
-            if (grid[i][j] === 1) neighboursAlive++
-        }
-    }
-    return neighboursAlive
+	var negs = 0
+	var max = grid.length - 1
+
+	var fromX = x - 1 > 0 ? x - 1 : 0
+	var fromY = y - 1 > 0 ? y - 1 : 0
+
+	var toX = x + 1 <= max ? x + 1 : max
+	var toY = y + 1 <= max ? y + 1 : max
+
+	for (var i = fromX; i <= toX; i++) {
+		for (var j = fromY; j <= toY; j++) {
+			if (i === x && j === y) continue
+			if (grid[i][j] === 1) negs++
+		}
+	}
+	return negs
 }
 
 function startGame() {
-    gGrid = playTurn()
-    stopGame(gGrid)
+	gGrid = playTurn()
+	stopGame(gGrid)
 }
 
 function playTurn() {
-    var newGrid = []
-    newGrid = getNextState(gGrid)
-    printGrid(newGrid)
-    return newGrid
+	var newGrid = []
+	newGrid = getNextState(gGrid)
+	printGrid(newGrid)
+	return newGrid
 }
 
 function stopGame(grid) {
-    NUMBER_OF_GENERATIONS--
-    if (NUMBER_OF_GENERATIONS === 0 || sumMatrix(grid) === 0) clearInterval(gIntervalId)
+	gNumOfGeneretions--
+	if (gNumOfGeneretions === 0 || isDead(grid)) clearInterval(gIntervalId)
 }
 
-function sumMatrix(grid) {
-    var retSum = 0
-    for (let i = 0; i < grid.length; i++) {
-        const row = grid[i];
-        for (let j = 0; j < row.length; j++) {
-            retSum += row[j];
-        }
-    }
-    return retSum
+function isDead(grid) {
+	for (let i = 0; i < grid.length; i++) {
+		for (let j = 0; j < grid[i].length; j++) {
+			if (grid[i][j]) return false
+		}
+	}
+	return true
 }
